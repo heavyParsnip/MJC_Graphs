e23af241394a8d5162f75b91835d7579ebcd5923
Matthew Camera <mjc9536@rit.edu>
2019-11-20 19:14:39 -0500

    PE Completed

---
+++ MJC_Graphs/Graph.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MJC_Graphs
+{
+    class Graph
+    {
+        //FIELDS
+        private Dictionary<String, List<String>> adjacency = new Dictionary<string, List<string>>();
+
+        //CONSTRUCTORS
+        public Graph()
+        {
+            List<string> courtyard = new List<string>()
+            {
+                "boiler room",
+                "foyer",
+                "library"
+            };
+            List<string> boiler = new List<string>()
+            {
+                "courtyard"
+            };
+            List<string> foyer = new List<string>()
+            {
+                "courtyard",
+                "library"
+            };
+            List<string> library = new List<string>()
+            {
+                "courtyard",
+                "foyer",
+                "archive",
+                "exit"
+            };
+            List<string> archive = new List<string>()
+            {
+                "library",
+                "exit"
+            };
+            List<string> exit = new List<string>()
+            {
+                "library",
+                "archive",
+                "exit"
+            };
+
+            adjacency.Add("courtyard", courtyard);
+            adjacency.Add("boiler room", boiler);
+            adjacency.Add("foyer", foyer);
+            adjacency.Add("library", library);
+            adjacency.Add("archive", archive);
+            adjacency.Add("exit", exit);
+        }
+
+        //METHODS
+        public List<string> GetAdjacentList(string room)
+        {
+            if(adjacency.ContainsKey(room) == true)
+            {
+                return adjacency[room];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool IsConnected(string room1, string room2)
+        {
+            if(adjacency[room1].Contains(room2) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MJC_Graphs/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MJC_Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize variables
+            Graph rooms = new Graph();
+            string currentRoom = "courtyard";
+            string input;
+
+            //Room loop
+            while (currentRoom != "exit")
+            {
+                Console.WriteLine($"\nYou are currently in the {currentRoom.ToUpper()}");
+                List<string> currentAdjacency = rooms.GetAdjacentList(currentRoom);
+                Console.Write($"\tNearby are: ");
+
+                foreach(string r in currentAdjacency)
+                {
+                    Console.Write($"\t{r} ");
+                }
+
+                Console.WriteLine("\n\tWhere would you like to go?");
+                input = Console.ReadLine().ToLower();
+
+                if (rooms.IsConnected(currentRoom, input) == true)
+                {
+                    currentRoom = input;
+                }
+                else
+                {
+                    while (rooms.IsConnected(currentRoom, input) == false)
+                    {
+                        Console.WriteLine("Sorry, that is not an adjacent room.");
+                        Console.WriteLine("\n\tWhere would you like to go?");
+                        input = Console.ReadLine().ToLower();
+                        if (rooms.IsConnected(currentRoom, input) == true)
+                        {
+                            currentRoom = input;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("\nYou have successfully escaped.");
+            Console.WriteLine("Press ENTER to continue. . .");
+            Console.ReadLine();
+        }
+    }
+}
